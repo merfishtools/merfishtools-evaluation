8dc74f81d4988f6f41e5c635f6b755cc31d82543
Johannes Koester <koester@jimmy.harvard.edu>
2016-05-11 06:24:29 -0400

    Fix diffexp analysis in case of zero significant genes.

--- scripts/go-enrichment.R
+++ scripts/go-enrichment.R
@@ -9,27 +9,33 @@ est <- read.table(snakemake@input[[1]], header = TRUE, stringsAsFactors = FALSE)
 # translate gene names to entrez ids
 ensembl = useMart("ensembl",dataset="hsapiens_gene_ensembl")
 ids <- getBM(attributes = c("hgnc_symbol", "entrezgene"), filters = "hgnc_symbol", values = est$feat, mart = ensembl)
+# take the first entrez id in case of ambiguity
+ids <- ids[!duplicated(ids$hgnc_symbol), ]
 rownames(ids) <- ids$hgnc_symbol
+# lookup entrez ids
 est$entrez <- ids[est$feat, "entrezgene"]
 
 # define foreground genes
 significant <- est$diff_fdr <= 0.05
 foreground <- est[significant, ]
+if(nrow(foreground) > 0) {
+    # define test parameters
+    params <- new("GOHyperGParams",
+                  geneIds = foreground$entrez,
+                  universeGeneIds = est$entrez,
+                  ontology = "BP",
+                  annotation = "org.Hs.eg",
+                  pvalueCutoff = 0.05,
+                  conditional = TRUE,
+                  testDirection = "over")
+    results <- hyperGTest(params)
+    goterms <- summary(results)
 
-# define test parameters
-params <- new("GOHyperGParams",
-              geneIds = foreground$entrez,
-              universeGeneIds = est$entrez,
-              ontology = "BP",
-              annotation = "org.Hs.eg",
-              pvalueCutoff = 0.05,
-              conditional = TRUE,
-              testDirection = "over")
-results <- hyperGTest(params)
-goterms <- summary(results)
+    # correct for multiple testing. We use Benjamini-Yekuteli here, because the performed tests are strongly dependent
+    by <- BY(goterms$Pvalue, 0.05)
+    goterms$adjPvalue <- by[["adjPValues"]]
 
-# correct for multiple testing. We use Benjamini-Yekuteli here, because the performed tests are strongly dependent
-by <- BY(goterms$Pvalue, 0.05)
-goterms$adjPvalue <- by[["adjPValues"]]
-
-write.table(goterms, file = snakemake@output[["table"]], row.names = FALSE, quote = FALSE, sep = "\t")
+    write.table(goterms, file = snakemake@output[["table"]], row.names = FALSE, quote = FALSE, sep = "\t")
+} else {
+    file.create(snakemake@output[["table"]])
+}
