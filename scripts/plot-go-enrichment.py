b9aaa8155ebc41982962297a5625e4f0cc7a2760
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-01-03 09:52:22 +0100

    Updated results (better credible intervals).

--- scripts/plot-go-enrichment.py
+++ scripts/plot-go-enrichment.py
@@ -15,11 +15,19 @@ terms = terms[terms["adjPvalue"] <= 0.05]
 
 genes = genes.loc[terms.index]
 genes = genes.set_index(["goterm", "gene"])
-matrix = ~genes["cv"].unstack().isnull()
+#matrix = ~genes["cv"].unstack().isnull()
+matrix = genes["cv"].unstack()
 matrix = matrix.loc[terms.index]
 matrix.sort_index(axis=1, inplace=True)
+
+annot = genes["fdr"].unstack()
+annot = annot.loc[terms.index]
+annot.sort_index(axis=1, inplace=True)
+annot[annot <= 0.05] = "•"
+annot[annot > 0.05] = ""
+
 plt.figure(figsize=(2,2))
-sns.heatmap(matrix, cmap=plt.cm.Greys, cbar=False)
+sns.heatmap(matrix, cmap=plt.cm.Greys, annot=annot)
 plt.ylabel("")
 plt.xlabel("")
 plt.savefig(snakemake.output[0], bbox_inches="tight")
