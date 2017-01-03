cd013478dcff2998d2775c654805e1f29f49d491
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-01-03 10:09:17 +0100

    Updated figures.

--- scripts/plot-go-enrichment.py
+++ scripts/plot-go-enrichment.py
@@ -11,7 +11,7 @@ terms = pd.read_table(snakemake.input.terms, index_col=0)
 genes = pd.read_table(snakemake.input.genes)
 genes.index = genes["goterm"]
 
-terms = terms[terms["adjPvalue"] <= 0.05]
+terms = terms[terms["adjPvalue"] <= 0.1]
 
 genes = genes.loc[terms.index]
 genes = genes.set_index(["goterm", "gene"])
@@ -19,15 +19,20 @@ genes = genes.set_index(["goterm", "gene"])
 matrix = genes["cv"].unstack()
 matrix = matrix.loc[terms.index]
 matrix.sort_index(axis=1, inplace=True)
+print(matrix)
 
 annot = genes["fdr"].unstack()
 annot = annot.loc[terms.index]
 annot.sort_index(axis=1, inplace=True)
-annot[annot <= 0.05] = "•"
-annot[annot > 0.05] = ""
+print(annot)
+significant = annot <= 0.05
+annot[significant] = "•"
+annot = annot.fillna(" ")
+print(annot)
+annot[~significant] = " "
 
-plt.figure(figsize=(2,2))
-sns.heatmap(matrix, cmap=plt.cm.Greys, annot=annot)
+plt.figure(figsize=(3,2))
+sns.heatmap(matrix, cmap=plt.cm.Reds, annot=annot, fmt="s", cbar_kws={"label": "CV"})
 plt.ylabel("")
 plt.xlabel("")
 plt.savefig(snakemake.output[0], bbox_inches="tight")
