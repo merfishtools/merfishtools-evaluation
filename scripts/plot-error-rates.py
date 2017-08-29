52ad89f19d05fd577bb60fa27f87d6176ed96a93
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-08-29 14:58:54 +0200

    Plotting.

--- scripts/plot-error-rates.py
+++ scripts/plot-error-rates.py
@@ -7,14 +7,24 @@ import seaborn as sns
 
 sns.set(style="ticks", palette="colorblind", context=snakemake.wildcards.context)
 x, y = snakemake.config["plots"]["figsize"]
-plt.figure(figsize=(x, y))
+plt.figure(figsize=(x * 1.5, y))
 
 
 error_rates = pd.concat([pd.read_table(f, index_col=0) for f in snakemake.input],
                         keys=snakemake.params.experiments)
+
 error_rates = pd.DataFrame(error_rates.stack()).reset_index()
-error_rates.columns = ["pos", "error", "rate"]
+error_rates.columns = ["experiment", "pos", "error-rate", "rate"]
 
-sns.barplot(x="pos", y="rate", hue="error")
-
+sns.barplot(x="pos", y="rate", hue="error-rate", data=error_rates, errwidth=1, linewidth=0, palette=["red", "grey"], saturation=0.7)
+plt.ylim((0.0, plt.ylim()[1] + 0.1))
+plt.ylabel("")
+sns.despine()
 plt.savefig(snakemake.output[0], bbox_inches="tight")
+
+print("mean error rates per position")
+means = error_rates.groupby(["error-rate", "pos"])["rate"].mean()
+print("p0", list(means.loc["p0"].values))
+print("p1", list(means.loc["p1"].values))
+print("mean overall")
+print(error_rates.groupby("error-rate")["rate"].mean())
