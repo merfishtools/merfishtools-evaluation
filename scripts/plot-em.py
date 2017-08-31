8f9fe7b1265151234a6ebacc4a8f64f123c8181d
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-08-31 09:37:08 +0200

    Add missing script.

---
+++ scripts/plot-em.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+mpl.use("svg")
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import re
+
+pattern = re.compile(r"DEBUG: TYPE=EM-iteration, CELL=(?P<cell>\d+), x=(?P<expr>\[(\d+, )+\d+\])")
+
+
+exprs = []
+
+with open(snakemake.input[0]) as f:
+    for l in f:
+        m = pattern.match(l)
+        if re.match(r"DEBUG: TYPE=EM-iteration, CELL=(?P<cell>\d+)", l):
+            print("match")
+        if m and m.group("cell") == "1":
+            exprs.append(eval(m.group("expr")))
+
+exprs = pd.DataFrame(exprs)
+
+
+sns.set(style="ticks", palette="colorblind", context=snakemake.wildcards.context)
+x, y = snakemake.config["plots"]["figsize"]
+plt.figure(figsize=(x * 1.5, y))
+
+for feat, e in exprs.iteritems():
+    plt.plot(np.arange(e.size), e, "k-", linewidth=1, alpha=0.5)
+
+sns.despine()
+
+plt.xlabel("EM iteration")
+plt.ylabel("expression")
+plt.ylim((0, 200))
+plt.savefig(snakemake.output[0], bbox_inches="tight")
